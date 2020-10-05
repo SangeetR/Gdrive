2c4ad75c4c44f9c7b915ddbcc69944a138c03504
sangeet <sangeetrathi1@gmail.com>
2020-10-05 20:49:50 +0530

    not much

--- hashcalculate.py
+++ hashcalculate.py
@@ -1,9 +1,11 @@
 import hashlib
 
-with open("Friends - [7x08] - The One Where Chandler Doesn't Like Dogs.mkv", "rb") as f:
+with open("Download\Friends - [7x11] - The One with All the Cheesecakes.mkv", "rb") as f:
     file_hash = hashlib.md5()
     while chunk := f.read(8192):
         file_hash.update(chunk)
 
 print(file_hash.digest())
 print(file_hash.hexdigest())
+9ad8881bcbc70a68c9e7b3986fce0ffc
+9ad8881bcbc70a68c9e7b3986fce0ffc
